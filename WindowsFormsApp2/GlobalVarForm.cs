151089431848f35ca8a69b96f8e8de0427690aa0
kiet <damrongkiet@hotmail.com>
2018-08-10 10:11:29 +0700

    test

--- WindowsFormsApp2/GlobalVarForm.cs
+++ WindowsFormsApp2/GlobalVarForm.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("close this form now.");
             Close();
         }
     }
