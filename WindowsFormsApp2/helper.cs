112aebb429e246efc4a06903dd9f6e9260dd6ddd
kiet <damrongkiet@hotmail.com>
2018-08-10 10:11:01 +0700

    add helper function

--- WindowsFormsApp2/helper.cs
+++ WindowsFormsApp2/helper.cs
@@ -9,4 +9,41 @@ namespace WindowsFormsApp2
     class helper
     {
     }
+    class Utility
+    {
+        public static string[] SplitString(string msg, string StrSplit)
+        {
+            return SplitString(msg, StrSplit, true);
+        }
+        public static string[] SplitString(string msg, string StrSplit, bool IsRemoveEmpty)
+        {
+            if (string.IsNullOrEmpty(msg)) { return new string[] { }; }
+            else if (IsRemoveEmpty)
+                return msg.Split(new string[] { StrSplit }, StringSplitOptions.RemoveEmptyEntries);
+            else { return msg.Split(new string[] { StrSplit }, StringSplitOptions.None); }
+        }
+        public class CompareDateTime : IComparer<string>//format => "dd/MM/yyyy HH:mm:ss"
+        {
+            public int Compare(string x, string y)
+            {
+                string[] arr_x = Utility.SplitString(x, " ");
+                string[] arr_y = Utility.SplitString(y, " ");
+                string[] date_x = Utility.SplitString(arr_x[0], "/");
+                string[] date_y = Utility.SplitString(arr_y[0], "/");
+                for (int i = 2; i >= 0; i--)
+                {
+                    if (date_x[i] != date_y[i])
+                        return string.Compare(date_x[i], date_y[i]);
+                }
+                string[] time_x = Utility.SplitString(arr_x[1], ":");
+                string[] time_y = Utility.SplitString(arr_y[1], ":");
+                for (int i = 0; i < 3; i++)
+                {
+                    if (time_x[i] != time_y[i])
+                        return string.Compare(time_x[i], time_y[i]);
+                }
+                return 0;
+            }
+        }
+    }
 }
